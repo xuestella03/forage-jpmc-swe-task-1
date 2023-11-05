d895f501fbfa92ace9a3dc9d501bc43cef0c3073
Estella Xu <xuestella03@gmail.com>
2023-11-04 21:27:32 -0500

    2

--- client3.py
+++ client3.py
@@ -42,7 +42,9 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if (price_b == 0):
+        return
+    return price_a/price_b
 
 
 # Main
